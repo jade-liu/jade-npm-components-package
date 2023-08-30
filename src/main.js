a3b66bfa4dc1c17edba05165c64b1b432b230a01
liuyu <2259023634@qq.com>
2023-08-30 09:01:57 +0800

    package npm publish

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import componentsBoxs from "../plugins/index.js"
 
 Vue.config.productionTip = false
+
+Vue.use(componentsBoxs)
 
 new Vue({
   render: h => h(App),
